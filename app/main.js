717371f6754b7840fb996e7fb9037db2c21f0e90
iceStone <ice@micua.com>
2016-10-29 12:08:58 +0800

    eslint

--- app/main.js
+++ app/main.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+
+
+
 import App from './app'
 import routes from './routes'
 
 import './assets/css/normalize.css'
 import './assets/less/global.less'
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const router = new VueRouter({
   // http://router.vuejs.org/en/api/options.html
